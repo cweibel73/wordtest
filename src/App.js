0079ffaa67282feebb44233e27ff7612962a480a
Carl Weibel <carleweibel@gmail.com>
2022-02-19 01:58:01 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [word, setWord] = useState('')
+    const [result, setResult] = useState('')
+
+    function handleChange(e) {
+        setWord(e.target.value)
+    }
+    function handleSubmit(e) {
+        e.preventDefault()
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+            .then(res => res.json())
+            .then(data => setResult(data[0] ?
+                "Yes, it is a word: " + data[0].meanings[0].definitions[0].definition :
+                "Nope,not a word."))
+
+    }
+    function reset() {
+        setWord('')
+        setResult('')
+    }
+    return (
+        <div className="App">
+            <h1>Is it a word?</h1>
+            {result}<br />
+            <form onSubmit={handleSubmit}>
+                Enter a word:<input type="text" onChange={handleChange} value={word} />
+                <button type="submit">Submit</button>
+                <button onClick={reset}>Reset</button>
+            </form>
+        </div>
+    );
+
 }
 
 export default App;
